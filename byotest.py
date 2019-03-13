098a99f515d82435c0b06539bec1e790ce7ddafd
Tom Holmes <thomasjj.holmes@gmail.com>
2019-03-13 14:19:31 +0000

    adding vending machne file

--- byotest.py
+++ byotest.py
@@ -47,19 +47,3 @@ def test_between(upper_limit, lower_limit, actual):
     AssertionError if the number is not between the other two numbers
     """
     assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
-
-
-# Test to fail the `test_are_equal` function
-# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
-
-# Test to fail the `test_not_equal` function
-# test_not_equal(0, 0)
-
-# Test to fail the `test_is_in` function
-# test_is_in([1], 2)
-
-# Test to fail the `test_not_in` function
-# test_not_in([1], 1)
-
-# Test to fail the `test_between` function
-test_between(10, 1, 200)
